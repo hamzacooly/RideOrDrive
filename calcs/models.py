6b654fff14724fda10c6fec3a8a1bf9f69e8bb14
Hamza Khatri <hamzakhatri@utexas.edu>
2018-11-14 14:15:46 -0600

    More backend

--- calcs/models.py
+++ calcs/models.py
@@ -4,12 +4,12 @@ from djongo import models
 
 class Calculation(models.Model):
     source = models.CharField(max_length=255)
-    source_lat = models.DecimalField(max_digits=20, decimal_places=12)
-    source_long = models.DecimalField(max_digits=20, decimal_places=12)
+    start_lat = models.DecimalField(max_digits=20, decimal_places=12)
+    start_long = models.DecimalField(max_digits=20, decimal_places=12)
 
     dest = models.CharField(max_length=255)
-    dest_lat = models.DecimalField(max_digits=20, decimal_places=12)
-    dest_long = models.DecimalField(max_digits=20, decimal_places=12)
+    end_lat = models.DecimalField(max_digits=20, decimal_places=12)
+    end_long = models.DecimalField(max_digits=20, decimal_places=12)
 
     uber_price = models.DecimalField(max_digits=10, decimal_places=6)
     lyft_price = models.DecimalField(max_digits=10, decimal_places=6)
@@ -18,6 +18,17 @@ class Calculation(models.Model):
         return "source: {} @ ({}, {})\n \
                 dest: {} @ ({}, {})\n \
                 uber_price: {}\n \
-                lyft_price: {}".format(self.source, self.source_lat, self.source_long, 
-                                        self.dest, self.dest_lat, self.dest_long, 
+                lyft_price: {}".format(self.source, self.start_lat, self.start_long, 
+                                        self.dest, self.end_lat, self.end_long, 
                                         self.uber_price, self.lyft_price) 
+
+class Input(models.Model):
+    start_lat = models.DecimalField(max_digits=20, decimal_places=12)
+    start_long = models.DecimalField(max_digits=20, decimal_places=12)
+    end_lat = models.DecimalField(max_digits=20, decimal_places=12)
+    end_long = models.DecimalField(max_digits=20, decimal_places=12)
+
+    def __str__(self):
+        return "source: ({}, {})\n \
+                dest: ({}, {})\n".format(self.start_lat, self.start_long,
+                                        self.end_lat, self.end_long)
--- calcs/views.py
+++ calcs/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .forms import InputForm
+from RideOrDrive.settings import secrets
+import os
+import geocoder
+
+os.environ["GOOGLE_API_KEY"] = secrets['google_api_key ']
 
 # Create your views here.
+
+def calc(request):
+    if request.method == "POST":
+        my_input = InputForm(request.POST)
+        if my_input.is_valid():
+            my_input.save()
+
+            # Now do some calculations
+            # Use Geocoding API to get location string of start and end
+            calc_data = my_input.cleaned_data
+            calc_data['source'] = geocoder.google([calc_data['start_lat'], calc_data['start_long']], method='reverse')
+            calc_data['dest'] = geocoder.google([calc_data['end_lat'], calc_data['end_long']], method='reverse')
+
+            # then use Uber + Lyft APIs to get the prices
+
+            return JsonResponse(calc_data)
+        else:
+            return HttpResponse(status=400)
+
+
