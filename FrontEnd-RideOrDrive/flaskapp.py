633443cb22f9e975fc44b42622ef0cea2df0d7e5
Hamza Khatri <hamzakhatri@utexas.edu>
2018-12-09 14:40:39 -0600

    Added in login manager + User stuff

--- FrontEnd-RideOrDrive/flaskapp.py
+++ FrontEnd-RideOrDrive/flaskapp.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, url_for, abort
+from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import os, json
 from pygeocoder import Geocoder
 from lyft_rides.auth import ClientCredentialGrant
@@ -8,8 +10,10 @@ from lyft_rides.session import Session as Session2
 from lyft_rides.client import LyftRidesClient
 
 app = Flask(__name__)
-secrets = json.loads(open('secrets.json', 'r').read())
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+secrets = json.loads(open('secrets.json', 'r').read())
 os.environ['GOOGLE_API_KEY'] = secrets['google_api_key']
 
 uberServerKey = secrets['uber-server-key']
@@ -31,6 +35,24 @@ auth_flow = ClientCredentialGrant(
 lyft_session = auth_flow.get_session()
 lyft_client = LyftRidesClient(lyft_session)
 
+class User(UserMixin):
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+        self.history = []
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+    
+    def add_history_item(self, item):
+        self.history.append(item)
+    
+    def get_history(self):
+        return self.history
 
 @app.route('/')
 @app.route('/static/index.html')
@@ -38,10 +60,20 @@ lyft_client = LyftRidesClient(lyft_session)
 def index():
     return render_template('web/index.html')
 
-@app.route('/login.html')
-@app.route('/static/login.html')
+@app.route('/login.html', methods = ['POST', 'GET'])
+@app.route('/static/login.html', methods = ['POST', 'GET'])
 def login():
-    return render_template('web/login.html')
+    if request.method == "GET":
+        return render_template('web/login.html')
+    else:
+        #TODO! Replace this with actual stuff
+        return abort(403)
+
+@app.route('/history.html')
+@login_required
+def history():
+    # Add in more stuff about getting the user's history!
+    return render_template('web/history.html')
 
 @app.route('/result', methods=['POST'])
 def result():
