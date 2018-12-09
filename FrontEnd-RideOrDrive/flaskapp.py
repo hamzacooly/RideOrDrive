bd8756e191b10ae22ec242a1b165745d430646bc
dhruvsand <dhruvsand@yahoo.com>
2018-12-09 14:31:37 -0600

    Added the parkign scraper display on results

--- FrontEnd-RideOrDrive/flaskapp.py
+++ FrontEnd-RideOrDrive/flaskapp.py
@@ -6,6 +6,8 @@ from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
 from lyft_rides.session import Session as Session2
 from lyft_rides.client import LyftRidesClient
+from parking_scraper import ParkMeScraper
+
 
 app = Flask(__name__)
 secrets = json.loads(open('secrets.json', 'r').read())
@@ -19,6 +21,11 @@ uber_client = UberRidesClient(uber_session)
 
 googleApiKey = secrets['google_api_key']
 geo = Geocoder(api_key=googleApiKey)
+
+
+
+
+
 
 
 auth_flow = ClientCredentialGrant(
@@ -49,13 +56,13 @@ def result():
         slat,slong = geo.geocode(source).coordinates
         dlat,dlong = geo.geocode(destination).coordinates
 
-
+        lots = ParkMeScraper().getLots(dlat, dlong)[:5]
 
 
         ride_estimates_uber = uber_client.get_price_estimates(slat, slong, dlat, dlong).json
         ride_estimates_lyft = lyft_client.get_cost_estimates(slat, slong, dlat, dlong).json
 
-        return render_template('web/result.html', source=source, destination=destination, uber=ride_estimates_uber, lyft=ride_estimates_lyft)
+        return render_template('web/result.html', source=source, destination=destination, uber=ride_estimates_uber, lyft=ride_estimates_lyft, lots=lots)
     else:
         print("HOW DID I DO A GET??")
         abort(403)
