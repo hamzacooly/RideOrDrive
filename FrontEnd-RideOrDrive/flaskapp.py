37019b0e5ce9f741a227ccc8c2bacc2c128fa3f8
Hamza Khatri <hamzakhatri@utexas.edu>
2018-12-09 21:57:09 -0600

    Logins without auth

--- FrontEnd-RideOrDrive/flaskapp.py
+++ FrontEnd-RideOrDrive/flaskapp.py
@@ -41,6 +41,34 @@ auth_flow = ClientCredentialGrant(
 lyft_session = auth_flow.get_session()
 lyft_client = LyftRidesClient(lyft_session)
 
+class User(UserMixin):
+
+    def __init__(self, username, password, history):
+        self.username = username
+        self.set_password(password)
+        self.history = history
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+    
+    def add_history_item(self, item):
+        self.history.append(item)
+        appDB.users[self.username]['history'].insert(item)
+    
+    def get_history(self):
+        return self.history
+    
+    @staticmethod
+    def query_user(id):
+        user = appDB.users.find_one({'_id': id})
+        if user:
+            return User(user._id, user.password, user.history)
+        else:
+            return None
+
 @login_manager.user_loader
 def load_user(id):
     return User.query_user(id)
