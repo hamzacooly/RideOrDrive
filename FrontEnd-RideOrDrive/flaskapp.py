1bc40737df2705aadb06e8ca5a462de5b89fb0f7
Hamza Khatri <hamzakhatri@utexas.edu>
2018-12-10 03:42:23 -0600

    YES

--- FrontEnd-RideOrDrive/flaskapp.py
+++ FrontEnd-RideOrDrive/flaskapp.py
@@ -227,4 +227,4 @@ def result():
                                                 lotsMarkers=lotsMarkers)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
